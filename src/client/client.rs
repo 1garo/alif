eb157ad0321564a6cadbd0957414bff5514ba616
1garo <alevardai427@gmail.com>
2023-07-18 16:28:49 -0300

    can now write and read messages on client

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,31 +1,40 @@
 use std::{
     io::prelude::*,
     net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream},
-    str::from_utf8,
     time::Duration,
-    vec,
+    vec, str::{from_utf8, from_utf8_mut},
 };
 
-fn main() -> std::io::Result<()> {
+use anyhow::{Context, Result, Ok};
+
+async fn handle_connection(mut stream: TcpStream, ip: Ipv4Addr, port: u16) -> Result<()> {
+    let ip = ip.to_string();
+    //let mut buf = bufstream::BufStream::new(stream);
+    loop {
+        let mut buffer = String::new();
+        let mut b = vec![0u8; 512];
+        // TODO: somehow this does not work unless is a println!
+        print!("{ip}:{port}> ");
+        let mut stdin = std::io::stdin().lock();
+        let mut stdout = std::io::stdout().lock();
+
+        stdout.flush().context("could not flush stdout")?;
+        stdin.read_line(&mut buffer).context("should read stdin")?;
+
+        stream.write(buffer.as_bytes()).context("should write on stream")?;
+        stream.read(&mut b).context("should read")?;
+        stream.flush()?;
+        println!("returned: {}", from_utf8(&b)?);
+    }
+}
+
+#[async_std::main]
+async fn main() -> Result<()> {
     let port = 34254;
     let ip = Ipv4Addr::new(127, 0, 0, 1);
     let socket = SocketAddr::new(IpAddr::V4(ip), port);
-    let stream = TcpStream::connect_timeout(&socket, Duration::new(1, 0));
-    match stream {
-        Ok(mut stream) => {
-            let ip = ip.to_string();
-            loop {
-                let mut buffer = String::new();
-                print!("{ip}:{port}> ");
-                let stdin = std::io::stdin();
-                let mut stdout = std::io::stdout();
-                let mut handle = stdin.lock();
-                stdout.flush()?;
-
-                handle.read_line(&mut buffer)?;
-                stream.write(buffer.as_bytes())?;
-            }
-        }
-        Err(error) => panic!("Problem opening the file: {:?}", error),
-    }
+    let stream = TcpStream::connect_timeout(&socket, Duration::new(1, 0))?;
+    handle_connection(stream, ip, port).await?;
+    Ok(())
 }
+
